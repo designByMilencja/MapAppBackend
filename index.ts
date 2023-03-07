69f771d4943da9bb860d51217d4fd81f12940678
Milena Pieńkosz <designbymilencja@gmail.com>
2023-03-07 14:07:58 +0000

    added router: AdRouter

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import rateLimit from 'express-rate-limit';
 import 'express-async-errors';
 import {handleError} from "./utils/errors";
+import {adRouter} from "./routers/ad.routers";
 
 const app = express();
 
@@ -10,9 +11,7 @@ app.use(cors({
     origin: 'http://localhost:3000',
 }));
 app.use(express.json());
-// app.get('/', (req,res)=>{
-//     throw new ValidationError('ups')
-// } )
+app.use('/ad', adRouter);
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
